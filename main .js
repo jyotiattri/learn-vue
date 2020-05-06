b00ab48785c969be9c557b615a2b65deda534c39
jyoti attri <jyotiatri22@gmail.com>
2020-05-06 18:09:56 +0530

    using methods and update button text on click

--- main .js
+++ main .js
@@ -2,5 +2,15 @@ new Vue({
     el: '#app',
     data: {
         greeting: 'Hello World',
+        user: 'Jyoti Attri',
+        city: 'Mandi',
+    },
+    methods: {
+        // instance methods
+        changeGreeting() { 
+            this.greeting = this.greeting === 'Hello World!' ? 
+            'What is up!' : 
+            'Hello World!'; 
+        }
     },
 });
