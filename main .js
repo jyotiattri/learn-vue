83cac2e46786890374b9ceaa1a458f23c1c39b67
jyoti attri <jyotiatri22@gmail.com>
2020-05-07 21:00:41 +0530

    added change greeting method

--- main .js
+++ main .js
@@ -1,26 +1,39 @@
-// new Vue({
-//     el: '#app',
-//     data: {
-//         greeting: 'Hello World',
-//         user: 'Jyoti Attri',
-//         city: 'Mandi',
-//     },
-//     methods: {
-//         addgreeting() {
-//             // greeting is not initialized
-//             this.greeting = 'Hello World!';
-//         }
+new Vue({
+    el: '#app',
+    data: {
+        greeting: '',
+        user: 'Jyoti Attri',
+        city: 'Mandi',
+        items: [
+            { message: 'foo' },
+            { message: 'bar' }
+        ]
+    },
+    methods: {
+        addgreeting() {
+            // greeting is not initialized
+            this.greeting = 'Hello World!';
+        },
+        changeGreeting() {
+            this.greeting = this.greeting === 'Hello World!'?
+            'What is up!':
+            'Hello World!';
+        },
+        add() {
+            this.items.push({message: 'hello'})
+        }
         
 
-//     },
-// });
-// vanilla Js Implementation
-let greetingTag = document.getElementsByTagName("h2")[0];
+    },
+});
 
-changeGreeting = () => {
-    if (greetingTag.textContent === 'Hello World!') {
-        greetingTag.textContent = 'What is up!';    
-    } else {
-        greetingTag.textContent = 'Hello World!';
-    }
-}
+// vanilla Js Implementation
+// let greetingTag = document.getElementsByTagName("h2")[0];
+
+// changeGreeting = () => {
+//     if (greetingTag.textContent === 'Hello World!') {
+//         greetingTag.textContent = 'What is up!';    
+//     } else {
+//         greetingTag.textContent = 'Hello World!';
+//     }
+// }
