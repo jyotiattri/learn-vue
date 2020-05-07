77919824bfb859fc3702322906c21e57b0f3047e
jyoti attri <jyotiatri22@gmail.com>
2020-05-07 13:14:06 +0530

    extract vanilla js from vue in simple js

--- main .js
+++ main .js
@@ -1,25 +1,26 @@
-new Vue({
-    el: '#app',
-    data: {
-        greeting: 'Hello World',
-        user: 'Jyoti Attri',
-        city: 'Mandi',
-    },
-    methods: {
-        addgreeting() {
-            // greeting is not initialized
-            this.greeting = 'Hello World!';
-        }
-        // vanilla Js Implementation
-        let greetingTag = document.getElementsByTagName("h2")[0];
+// new Vue({
+//     el: '#app',
+//     data: {
+//         greeting: 'Hello World',
+//         user: 'Jyoti Attri',
+//         city: 'Mandi',
+//     },
+//     methods: {
+//         addgreeting() {
+//             // greeting is not initialized
+//             this.greeting = 'Hello World!';
+//         }
+        
 
-        changeGreeting = () => {
-            if (greetingTag.textContent === 'Hello World!') {
-                greetingTag.textContent = 'What is up!';    
-            } else {
-                greetingTag.textContent = 'Hello World!';
-            }
-        }
+//     },
+// });
+// vanilla Js Implementation
+let greetingTag = document.getElementsByTagName("h2")[0];
 
-    },
-});
+changeGreeting = () => {
+    if (greetingTag.textContent === 'Hello World!') {
+        greetingTag.textContent = 'What is up!';    
+    } else {
+        greetingTag.textContent = 'Hello World!';
+    }
+}
