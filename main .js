efa7835c4f0ed5bae58f38ae0ac7716837119ec7
jyoti attri <jyotiatri22@gmail.com>
2020-05-13 12:42:34 +0530

    simple method

--- main .js
+++ main .js
@@ -1,18 +1,7 @@
 new Vue({
     el: '#app',
     data: {
-        name: '',
-        subject: '',
-        termsAndConditions: false,
-        yesOrNo: 'No'
+       message: 'Greetings!'
     },
-    methods: {
-        submit() {
-            console.log('name', this.name);
-            console.log('subject', this.subject);
-            console.log('termsAndConditions', this.termsAndConditions);
-            console.log('yesOrNo', this.yesOrNo);
-
-        }
-    }
+   
 });
