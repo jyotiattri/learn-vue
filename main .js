3839b8d79483cc160128ae7ddba24608757acf49
jyoti attri <jyotiatri22@gmail.com>
2020-05-14 14:58:58 +0530

    added watchers

--- main .js
+++ main .js
@@ -1,15 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-       message: 'Greetings!'
+      km: 0,
+      m: 0
     },
-//    methods: {
-//        reverseString(string) {
-//            return string.split('').reverse().join('');
-//        },
-computed: {
-    reverseMessage() {
-        return this.message.split('').reverse().join('');
-},
+watch: {
+    km() {
+        // whenever km changes, this function runs
+        },
+        m() {
+            //whenever m changes -this will runs
+        }
    }
 });
