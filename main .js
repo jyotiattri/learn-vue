bbeba151200f577afa224fa2e73f79b602538366
jyoti attri <jyotiatri22@gmail.com>
2020-05-15 11:44:50 +0530

    added card details

--- main .js
+++ main .js
@@ -1,14 +1,11 @@
 new Vue({
   el: "#app",
-  data: { km: 0 },
-  computed: {
-    m: {
-      get() {
-        return this.km * 1000;
-      },
-      set(newValue) {
-        this.km = newValue / 1000;
-      },
-    },
-  },
+  data: { 
+    name: '',
+    email: '',
+    company: {
+      name: '',
+      catchPhrase: '',
+    }
+   },
 });
