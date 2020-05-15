e2631b9f851d0e4acea234519c906900959deb0e
jyoti attri <jyotiatri22@gmail.com>
2020-05-15 13:00:25 +0530

    changes in main.js

--- main .js
+++ main .js
@@ -1,11 +1,22 @@
 new Vue({
   el: "#app",
   data: { 
-    name: '',
-    email: '',
-    company: {
-      name: '',
-      catchPhrase: '',
+    // ...
     }
    },
+   created()
+ {
+   axios.get(
+     'https://jsonplaceholder.typicode.com/users'
+   ) .then((response) => {
+     const data = response.data;
+     const randomUser = response.data[
+       Math.floor(Math.random() * data.length)
+     ];
+     this.name = randomUser.name;
+     this.email = randomUser.email;
+     this.company = randomUser.company;
+     this.company.catchPhrase = randomUser.company.catchPhrase;
+   });
+ },
 });
