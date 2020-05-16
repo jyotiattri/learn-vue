64a7f42b342a53c19040b2f5ed78a850621223e8
jyoti attri <jyotiatri22@gmail.com>
2020-05-16 17:47:09 +0530

    added template in main.js

--- main .js
+++ main .js
@@ -1,32 +1,73 @@
-   const tweets = [
-         {
-           id: 1,
-           name: 'James',
-           handle: '@jokerjames',
-           img: './images/photo.jpg',
-           tweet: "If you don't succed, dust yourself off...",
-           likes: 10,
-         },
-        {     
-          id: 2,   
-          name: 'Fatima',
-          handle: '@fantasticfatima',    
-          img: './images/photo.jpg',   
-          tweet: 'Better late than never but never late...',    
-          likes: 12, 
-        }, 
-        {    
-          id: 3,    
-          name: 'Xin',    
-          handle: '@xeroxin',    
-          img: './images/photo.jpg',    
-          tweet: 'Beauty in the struggle...',    
-          likes: 18, 
-        }
-       ];
-       new Vue({
-        el: '#app',
-          data: {
-             tweets
-             }
-             });
+const tweets = [
+  {
+    id: 1,
+    name: "James",
+    handle: "@jokerjames",
+    img: "./images/photo.jpg",
+    tweet: "If you don't succed, dust yourself off...",
+    likes: 10,
+  },
+  {
+    id: 2,
+    name: "Fatima",
+    handle: "@fantasticfatima",
+    img: "./images/photo.jpg",
+    tweet: "Better late than never but never late...",
+    likes: 12,
+  },
+  {
+    id: 3,
+    name: "Xin",
+    handle: "@xeroxin",
+    img: "./images/photo.jpg",
+    tweet: "Beauty in the struggle...",
+    likes: 18,
+  },
+];
+Vue.component("tweet-component", {
+  template: `      
+   <div class="tweet">      
+   <div class="box">        
+   <article class="media">         
+    <div class="media-left">          
+      <figure class="image is-64x64">      
+              <img src="./images/photo.jpg">       
+                   </figure>         
+                    </div>         
+                     <div class="media-content">           
+                      <div class="content">           
+                         <p>               
+                          <strong>James</strong>               
+                           <small>@jokerjames</small>              
+                             <br>              
+                               If you don't succeed, dust yourself off...             
+                                </p>           
+                                 </div>          
+                                   <div class="level-left">     
+                                            <a class="level-item">         
+                                                   <span class="icon is-small"> 
+                                                        <i class="fas fa-heart"></i>               
+                                                         </span>               
+                                                          <span class="likes">                  
+                                                          10                
+                                                          </span>              
+                                                          </a>           
+                                                           </div>          
+                                                           </div>        
+                                                           </article>     
+                                                            </div>     
+                                                             <div class="control has-icons-left">
+             <input class="input is-small"        
+               placeholder="Tweet your reply..." /> 
+                      <span class="icon is-small is-left"> 
+                               <i class="fas fa-envelope"></i>        
+                               </span>      
+                               </div>    
+                               </div>  `,
+});
+new Vue({
+  el: "#app",
+  data: {
+    tweets,
+  },
+});
