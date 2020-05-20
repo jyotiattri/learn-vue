1e225fa3c1162a6b54bdef11aa107c2a3e838914
jyoti attri <jyotiatri22@gmail.com>
2020-05-20 11:22:11 +0530

    standred single template

--- main .js
+++ main .js
@@ -1,135 +1,7 @@
-// const tweets = [
-//   {
-//     id: 1,
-//     name: "James",
-//     handle: "@jokerjames",
-//     img: "./images/photo.jpg",
-//     tweet: "If you don't succed, dust yourself off...",
-//     likes: 10,
-//   },
-//   {
-//     id: 2,
-//     name: "Fatima",
-//     handle: "@fantasticfatima",
-//     img: "./images/photo.jpg",
-//     tweet: "Better late than never but never late...",
-//     likes: 12,
-//   },
-//   {
-//     id: 3,
-//     name: "Xin",
-//     handle: "@xeroxin",
-//     img: "./images/photo.jpg",
-//     tweet: "Beauty in the struggle...",
-//     likes: 18,
-//   },
-// ];
-// Vue.component("tweet-content", {
-//   template: `
-//   <div class="media-content">
-//   <div class="content">
-//   <p>
-//   <strong>{{ tweet.name }}</strong>
-//   <small>{{ tweet.handle }}</small>
-//   <br>
-//   {{tweet.tweet}}
-//   </p>
-//   </div>
-//   <div class="level-left">
-//   <a class="level-item">
-//   <span class="icon is-small">
-//   <i class="fas fa-heart"></i>
-//   </span>
-//   <span class="likes">{{ tweet.likes }}</span>
-//   </a>
-//   <a class="level-item"
-//   @click="$emit('add', tweet)">
-//   <span class="icon is-small">
-//   <i class="fas fa-plus"></i>
-//   </span>
-//   </a>
-//   </div>
-//   </div>
-//   `,
-//   props: ["tweet"],
-// });
-// Vue.component("tweet-component", {
-//   template: `      
-//    <div class="tweet">      
-//    <div class="box">        
-//    <article class="media">         
-//     <div class="media-left">          
-//       <figure class="image is-64x64">      
-//               <img src="./images/photo.jpg">       
-//                    </figure>         
-//                     </div>  
-//                     <tweet-content :tweet="tweet"
-//                     @add="$emit('add', $event)"></tweet-content>       
-                 
-//                                   </article>     
-//                                       </div>     
-//                   <div class="control has-icons-left">
-//              <input class="input is-small"        
-//                placeholder="Tweet your reply..." /> 
-//                       <span class="icon is-small is-left"> 
-//                                <i class="fas fa-envelope"></i>        
-//                                </span>      
-//                                </div>    
-//                                </div>  `,
-//   props: ["tweet"],
-// });
-// new Vue({
-//   el: "#app",
-//   data: {
-//     tweets,
-//   },
-//   methods: {
-//     addTweetMessage(tweet) {
-//       let newTweet ={};
-//       let lastTweetObjectID =
-//       this.tweets[this.tweets.length -1].id
-//       newTweet = Object.assign({}, tweet);
-//       newTweet.id = lastTweetObjectID +1;
-
-//     }
-//   }
-// });
-
-let counterOne={
-  template: `
-  <button class="component-button" @click="counter++">
-  You clicked me {{counter}} times
-  </button>
-  `,
-  data() {
-return {
-  counter: 0
-}
-  }
-}
-let counterTwo={
-  template: `
-  <button class="component-button" @click="counter++">
-  You clicked me {{counter}} times
-  </button>
-  `,
-  data() {
-    return {
-      counter: 0
-    }
-  }
-}
-let counterThree={
-  template: `
-  <button class="component-button" @click="counter++">
-  You clicked me {{counter}} times
-  </button>
-  `,
-  data() {
-    return {
-      counter:0
-    }
-  }
+let singleLineTemplate = {
+template: '<div class="card"><header class="card-header\
+card-header-title">{{ message }}</header></div>',
+props: ['message']
 }
 
 new Vue({
@@ -138,8 +10,7 @@ new Vue({
     message: 'Greetings!',
   },
   components: {
-    'counter-one': counterOne,
-    'counter-two': counterTwo,
-    'counter-three': counterThree
+    'single-line-template': singleLineTemplate
   }
 });
+
